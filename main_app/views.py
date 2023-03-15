40fbe3b1c6e6f93bb60b0912716feb471c53afc8
Daevah Taylaran <daevtayl@gmail.com>
2023-03-15 10:49:27 -0700

    working finch list with simulated data

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+finches = [
+  {'name': 'Leo', 'breed': 'finch1', 'description': 'fun bird'},
+  {'name': 'Luna', 'breed': 'finch2', 'description': 'chill bird'},
+  ]
+
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+# Add new view
+def finch_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'finch/index.html', {
+    'finches': finches
+  })
