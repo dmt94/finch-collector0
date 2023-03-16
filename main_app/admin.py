3b50ff69a974f0b79ab418a6110c567bf160592b
Daevah Taylaran <daevtayl@gmail.com>
2023-03-15 18:30:45 -0700

    update finch feeding table

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Finch
+from .models import Feeding
 
 # Register your models here
 admin.site.register(Finch)
+admin.site.register(Feeding)
